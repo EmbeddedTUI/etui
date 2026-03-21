cf3a2db93b7d5a55dbc99b959d0f7b90a57189ae
Pawel Wodnicki <pawel.wodnicki@live.com>
2026-03-20 20:31:23 -0500

    added EtuiApp

--- etui/main.py
+++ etui/main.py
@@ -1,8 +1,22 @@
 # Copyright (c) 2026 Pawel Wodnicki
 # Copyright (c) 2026 32bitmico LLC
 
+
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+
+class EtuiApp(App):
+    """ Embedded TUI App"""
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+
 def main():
     print("Hello from etui!")
+    app = EtuiApp()
+    app.run()
 
 if __name__ == "__main__":
     main()
