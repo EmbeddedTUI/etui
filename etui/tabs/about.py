5c7f070b35bedf518630cc0a9d08a8363073bc76
Pawel Wodnicki <pawel.wodnicki@live.com>
2026-06-05 23:58:05 -0500

    update copyright

--- etui/tabs/about.py
+++ etui/tabs/about.py
@@ -9,4 +9,4 @@ class AboutTab(Vertical):
     """ About tab"""
 
     def compose(self) -> ComposeResult:
-        yield Static("etui - (c) 2026")
+        yield Static("etui - (c) 32bitmico LLC 2026")
